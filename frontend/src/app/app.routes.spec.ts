2e015cccf45e1c7e3d31ee89fa9c3489ce2ded53
Rosmary <rosmarygp11@gmail.com>
2026-09-21 14:23:57 +0200

    feat: add request details with robust route handling

--- frontend/src/app/app.routes.spec.ts
+++ frontend/src/app/app.routes.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { provideRouter, Router } from '@angular/router';
 import { RouterTestingHarness } from '@angular/router/testing';
 import { routes } from './app.routes';
-import { of } from 'rxjs';
+import { NEVER, of } from 'rxjs';
 import { RequestService } from './requests/services/request.service';
 import { RequestList } from './requests/pages/request-list';
 import { RequestDetails } from './requests/pages/request-details';
@@ -11,7 +11,7 @@ import { RequestForm } from './requests/pages/request-form';
 describe('Request routes', () => {
   beforeEach(() => TestBed.configureTestingModule({ providers: [
     provideRouter(routes),
-    { provide: RequestService, useValue: { findAll: () => of([]) } },
+    { provide: RequestService, useValue: { findAll: () => of([]), findById: () => NEVER } },
   ] }));
 
   it('redirects the root to the request list', async () => {
